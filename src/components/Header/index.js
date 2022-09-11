693323bb8075c62e4f60e7771e8dbd659a1d8072
Rafael Arantes <97065934+365rafael@users.noreply.github.com>
2022-09-10 21:44:01 -0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import "./header.css";
 function Header() {
   return (
     <header>
-      <Link className="logo" to="/">
+      <Link className="logo" to="/filmesCinemaReact">
         Prime Flix
       </Link>
       <Link className="favoritos" to="/favoritos">
